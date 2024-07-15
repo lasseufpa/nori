7a445803445dfcbb9f8c3bf7930921f7dfa15045
Joao Albuquerque <joao.barbosa.albuquerque@itec.ufpa.br>
2024-07-15 15:27:30 -0300

    e2-term-helper: Add API class to help with E2 installation

---
+++ helper/E2-term-helper.cc
@@ -0,0 +1,268 @@
+#include "E2-term-helper.h"
+
+#include "ns3/antenna-module.h"
+#include "ns3/config-store.h"
+#include "ns3/core-module.h"
+#include "ns3/eps-bearer-tag.h"
+#include "ns3/grid-scenario-helper.h"
+#include "ns3/internet-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/log.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/nr-helper.h"
+#include "ns3/nr-module.h"
+#include "ns3/nr-point-to-point-epc-helper.h"
+#include <ns3/E2-report.h>
+#include <ns3/config.h>
+#include <ns3/log.h>
+#include <ns3/lte-enb-net-device.h>
+#include <ns3/lte-enb-rrc.h>
+#include <ns3/lte-pdcp.h>
+#include <ns3/lte-rlc.h>
+#include <ns3/nr-gnb-net-device.h>
+#include <ns3/nstime.h>
+#include <ns3/object-map.h>
+#include <ns3/object.h>
+#include <ns3/oran-interface.h>
+#include <ns3/pointer.h>
+#include <ns3/simulator.h>
+#include <ns3/string.h>
+#include <ns3/type-id.h>
+#include <ns3/uinteger.h>
+
+namespace ns3
+{
+
+NS_LOG_COMPONENT_DEFINE("E2TermHelper");
+NS_OBJECT_ENSURE_REGISTERED(E2TermHelper);
+
+E2TermHelper::E2TermHelper()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+E2TermHelper::~E2TermHelper()
+{
+    NS_LOG_FUNCTION(this);
+}
+
+TypeId
+E2TermHelper::GetTypeId()
+{
+    static TypeId tid = TypeId("ns3::E2TermHelper")
+                            .SetParent<Object>()
+                            .AddConstructor<E2TermHelper>()
+                            .AddAttribute("E2Term",
+                                          "E2 term creation, using the eNB/gNB net device; creates "
+                                          "an instance of the E2 term.",
+                                          PointerValue(),
+                                          MakePointerAccessor(&E2TermHelper::m_e2Term),
+                                          MakePointerChecker<E2Termination>())
+                            .AddAttribute("E2TermIp",
+                                          "The IP address of the RIC E2 termination",
+                                          StringValue("10.0.2.10"),
+                                          MakeStringAccessor(&E2TermHelper::m_e2ip),
+                                          MakeStringChecker())
+                            .AddAttribute("E2Port",
+                                          "Port number for E2",
+                                          UintegerValue(36422),
+                                          MakeUintegerAccessor(&E2TermHelper::m_e2port),
+                                          MakeUintegerChecker<uint16_t>())
+                            .AddAttribute("E2LocalPort",
+                                          "The first port number for the local bind",
+                                          UintegerValue(38470),
+                                          MakeUintegerAccessor(&E2TermHelper::m_e2localPort),
+                                          MakeUintegerChecker<uint16_t>());
+    return tid;
+}
+
+void
+E2TermHelper::InstallE2Term(Ptr<NetDevice> NetDevice)
+{
+    NS_LOG_FUNCTION(this);
+
+    // Create E2 messages scheduling
+    auto e2Messages = CreateObject<E2Interface>(NetDevice);
+
+    // Create E2 report
+    m_e2Report = CreateObject<NoriE2Report>();
+
+    // NetDevice is a gNB or eNB
+    auto nrGnbNetDev = DynamicCast<NrGnbNetDevice>(NetDevice);
+    // auto lteEnbNetDev = DynamicCast<LteEnbNetDevice>(NetDevice);
+
+    // Public Land Mobile Network Identifier or with abbreviated version PLMN is a combination of
+    // MCC and MNC. It is unique value and globally used to identify the mobile network that a user
+    // subscribed.
+    std::string plmnId = "111";
+    // node cell ID
+    uint16_t cellId{0};
+    // Client local port
+    uint16_t localPort{0};
+
+    // Enable E2 traces
+    EnableE2PdcpTraces();
+    EnableE2RlcTraces();
+
+    if (nrGnbNetDev != nullptr)
+    {
+        cellId = nrGnbNetDev->GetCellId();
+        NS_LOG_DEBUG("Cell ID: " << cellId);
+        localPort = m_e2localPort + cellId;
+        e2Messages->SetE2PdcpStatsCalculator(m_e2PdcpStats);
+        e2Messages->SetE2RlcStatsCalculator(m_e2RlcStats);
+    }
+    // else if (lteEnbNetDev != nullptr)
+    //{
+    //     cellId = lteEnbNetDev->GetCellId();
+    //     localPort = m_e2localPort + cellId;
+    //     // e2Messages->SetE2PdcpStatsCalculator(m_e2LtePdcpStats);
+    //     // e2Messages->SetE2RlcStatsCalculator(m_e2LteRlcStats);
+    // }
+    else
+    {
+        NS_ABORT_MSG("NetDevice is not a gNB or eNB");
+    }
+    // Assert that configuration was properly set
+    NS_ASSERT(plmnId == "111" && cellId != 0 && localPort != 0);
+
+    auto e2Term = CreateObject<E2Termination>(m_e2ip,
+                                              m_e2port,
+                                              m_e2localPort,
+                                              std::to_string(cellId),
+                                              plmnId);
+    NetDevice->AggregateObject(e2Term);
+    e2Messages->SetAttribute("E2Term", PointerValue(e2Term));
+
+    // Connect PDU's packets to callback report, after UEs registration.
+    // The schedule ensures that the connection is made after the UEs are registered.
+    Simulator::Schedule(Seconds(0.2),
+                        &E2TermHelper::ConnectPDUReports,
+                        this,
+                        NetDevice,
+                        e2Messages);
+    // Connect PHY traces
+    ConnectPhyTraces();
+    // Enable SINR traces
+    EnableSinrTraces(e2Messages);
+
+    // Connect E2 termination to E2 messages
+    if (!m_e2ForceLog)
+    {
+        Ptr<KpmFunctionDescription> kpmFd = Create<KpmFunctionDescription>();
+        e2Term->RegisterKpmCallbackToE2Sm(
+            200,
+            kpmFd,
+            std::bind(&E2Interface::KpmSubscriptionCallback, e2Messages, std::placeholders::_1));
+
+        Simulator::Schedule(MicroSeconds(0), &E2Termination::Start, e2Term);
+    }
+    else
+    {
+        ForceE2Log(NetDevice);
+    }
+
+    NetDevice->AggregateObject(e2Messages);
+}
+
+void
+E2TermHelper::InstallE2Term(NetDeviceContainer& NetDevices)
+{
+    NS_LOG_FUNCTION(this);
+    for (size_t i = 0; i < NetDevices.GetN(); i++)
+    {
+        InstallE2Term(NetDevices.Get(i));
+    }
+}
+
+void
+E2TermHelper::EnableE2PdcpTraces()
+{
+    NS_LOG_FUNCTION(this);
+    // Assert that the E2 report is going to be called once
+    // if (m_e2PdcpStats == nullptr && m_e2LtePdcpStats == nullptr)
+    //{
+    // Enable E2 PDCP traces
+    m_e2PdcpStats = CreateObject<NrBearerStatsCalculator>("E2PDCP");
+    m_e2PdcpStats->SetAttribute("DlPdcpOutputFilename", StringValue("DlE2PdcpStats.txt"));
+    m_e2PdcpStats->SetAttribute("UlPdcpOutputFilename", StringValue("UlE2PdcpStats.txt"));
+    m_e2StatsConnector.EnablePdcpStats(m_e2PdcpStats);
+
+    // m_e2Report->EnableE2PdcpStats(m_e2PdcpStats);
+
+    // Enable E2 PDCP traces for LTE
+    // m_e2LtePdcpStats = CreateObject<NrBearerStatsCalculator>("E2PDCPLTE");
+    // m_e2LtePdcpStats->SetAttribute("DlPdcpOutputFilename",
+    // StringValue("DlE2PdcpStatsLte.txt"));
+    // m_e2LtePdcpStats->SetAttribute("UlPdcpOutputFilename",
+    // StringValue("UlE2PdcpStatsLte.txt")); m_e2LtePdcpStats->SetAttribute("EpochDuration",
+    // TimeValue(Seconds(1))); m_e2Report->EnableE2PdcpStats(m_e2LtePdcpStats);
+    //}
+}
+
+void
+E2TermHelper::EnableE2RlcTraces()
+{
+    NS_LOG_FUNCTION(this);
+    // Assert that the E2 report is going to be called once
+    // if (m_e2RlcStats == nullptr && m_e2LteRlcStats == nullptr)
+    //{
+    // Enable E2 RLC traces
+    m_e2RlcStats = CreateObject<NrBearerStatsCalculator>("E2RLC");
+    m_e2RlcStats->SetAttribute("DlRlcOutputFilename", StringValue("DlE2RlcStats.txt"));
+    m_e2RlcStats->SetAttribute("UlRlcOutputFilename", StringValue("UlE2RlcStats.txt"));
+    m_e2StatsConnector.EnableRlcStats(m_e2RlcStats);
+
+    // Enable E2 RLC traces for LTE
+    // m_e2LteRlcStats = CreateObject<NrBearerStatsCalculator>("E2RLCLTE");
+    // m_e2LteRlcStats->SetAttribute("DlRlcOutputFilename", StringValue("DlE2RlcStatsLte.txt"));
+    // m_e2LteRlcStats->SetAttribute("UlRlcOutputFilename", StringValue("UlE2RlcStatsLte.txt"));
+    // m_e2LteRlcStats->SetAttribute("EpochDuration", TimeValue(Seconds(1)));
+    // m_e2Report->EnableE2RlcStats(m_e2LteRlcStats);
+    //}
+}
+
+void
+E2TermHelper::EnableSinrTraces(Ptr<E2Interface> e2Messages)
+{
+    NS_LOG_FUNCTION(this);
+    Config::ConnectFailSafe("/NodeList/*/DeviceList/*/ComponentCarrierMapUe/*/NrUePhy/DlDataSinr",
+                            MakeCallback(&E2Interface::RegisterNewSinrReadingCallback, e2Messages));
+}
+
+void
+E2TermHelper::ForceE2Log(Ptr<NetDevice> nodeB)
+{
+    NS_FATAL_ERROR("Not implemented yet");
+
+    /**
+     *
+        Simulator::Schedule(MicroSeconds(500),
+                            &E2Interface::BuildAndSendReportMessage,
+                            m_e2Interface,
+                            E2Termination::RicSubscriptionRequest_rval_s{},
+                            NetDevice);
+     */
+}
+
+void
+E2TermHelper::ConnectPDUReports([[maybe_unused]] Ptr<NetDevice> NetDevice,
+                                [[maybe_unused]] Ptr<E2Interface> e2Message)
+{
+    NS_LOG_FUNCTION(this << "Connecting PDU reports in E2 interface");
+
+    Config::ConnectWithoutContext(
+        "/NodeList/*/DeviceList/*/LteEnbRrc/UeMap/*/DataRadioBearerMap/*/LteRlc/TxPDU",
+        MakeCallback(&E2Interface::ReportTxPDU, e2Message));
+}
+
+void
+E2TermHelper::ConnectPhyTraces()
+{
+    Config::Connect(
+        "/NodeList/*/DeviceList/*/ComponentCarrierMapUe/*/NrUePhy/SpectrumPhy/RxPacketTraceUe",
+        MakeCallback(&NoriE2Report::UpdateTraces, m_e2Report));
+}
+
+} // namespace ns3
---
+++ helper/E2-term-helper.h
@@ -0,0 +1,121 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+
+// Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+#ifndef E2_TERM_H
+#define E2_TERM_H
+
+#include <ns3/E2-interface.h>
+#include <ns3/lte-enb-net-device.h>
+#include <ns3/net-device-container.h>
+#include <ns3/net-device.h>
+#include <ns3/nr-bearer-stats-calculator.h>
+#include <ns3/nr-bearer-stats-connector.h>
+#include <ns3/nr-gnb-net-device.h>
+#include <ns3/object-factory.h>
+#include <ns3/oran-interface.h>
+
+namespace ns3
+{
+class NoriE2Report;
+
+class E2TermHelper : public Object
+{
+  public:
+    /**
+     * \brief E2 term creation, using the eNB/gNB net device. It creates an instance of the E2 term;
+     * Also, installs it on the gNB or eNB node, with attributes.
+     */
+
+    E2TermHelper();
+
+    /**
+     * \brief ~NoriHelper
+     */
+    ~E2TermHelper() override;
+
+    /**
+     * \brief GetTypeId
+     * \return the type id of the object
+     */
+    static TypeId GetTypeId();
+
+    /**
+     * \brief Install E2 term on the net device.
+     * \param NetDevice the net device of the node where the E2 term will be installed.
+     *
+     */
+    void InstallE2Term(Ptr<NetDevice> NetDevice);
+
+    /**
+     * \brief Install E2 term on the
+     * \param NetDevice the net device of the node where the E2 term will be installed.
+     *
+     */
+    void InstallE2Term(NetDeviceContainer& NetDevices);
+
+    /**
+     * \brief Enable SINR traces
+     * \param e2Messages A pointer to the E2 interface
+     */
+    void EnableSinrTraces(Ptr<E2Interface> e2Messages);
+
+    /**
+     * \brief Force E2 Logging for the E2 termination, this methods calls E2Termination::Start
+     * \param NetDevice the E2 gNB/eNB net device will have a log.
+     *
+     */
+    void ForceE2Log(Ptr<NetDevice> NetDevice);
+
+    void SetStandardLog(bool standardLog);
+
+  private:
+    /***
+     * \brief Enable E2 RLC traces and report for LTE and NR cells.
+     */
+    void EnableE2RlcTraces();
+
+    /***
+     * \brief Enable E2 PDCP traces and report for LTE and NR cells.
+     */
+    void EnableE2PdcpTraces();
+
+    /**
+     * \brief Connect PDU reports to the E2 termination
+     * \param NetDevice the net device that provides the PDU information.
+     * \param e2Messages the E2 interface that will receive the PDU information.
+     */
+    void ConnectPDUReports(Ptr<NetDevice> NetDevice, Ptr<E2Interface> e2Messages);
+
+    /**
+     * \brief Connect PHY traces to the E2
+     *
+     */
+    void ConnectPhyTraces();
+
+    Ptr<NrBearerStatsCalculator> m_e2RlcStats;     // !< E2 RLC stats
+    Ptr<NrBearerStatsCalculator> m_e2PdcpStats;    // !< E2 PDCP stats
+    Ptr<NrBearerStatsCalculator> m_e2LteRlcStats;  // !< E2 LTE RLC stats
+    Ptr<NrBearerStatsCalculator> m_e2LtePdcpStats; // !< E2 LTE PDCP stats
+
+    NrBearerStatsConnector m_e2StatsConnector; // !< E2 PDCP connector
+
+    // E2 termination attributes
+    std::string m_e2ip;     // !< E2 termination IP
+    uint16_t m_e2port;      // !< E2 termination port
+    uint16_t m_e2localPort; // !< E2 termination local port
+
+    // E2 messages attributes
+    bool m_e2ForceLog;  // !< E2 force log
+    bool m_standardLog; // !< E2 standard message log
+
+    Ptr<E2Termination> m_e2Term;    // !< E2 termination
+    Ptr<E2Interface> m_e2Interface; // !< E2 interface
+    Ptr<NoriE2Report> m_e2Report;   // !< E2 report
+};
+
+} // namespace ns3
+
+#endif /* E2_TERM_H */
