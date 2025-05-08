3c228652f36a73b20eb5830cafa4547fae926aaa
AndreyAdailso <andreyadailsom@gmail.com>
2025-05-08 01:39:22 +0000

    feat: Add support for PDCP throughput in the indication message helper

--- helper/mmwave-indication-message-helper.h
+++ helper/mmwave-indication-message-helper.h
@@ -29,7 +29,7 @@ class MmWaveIndicationMessageHelper : public IndicationMessageHelper
 
     void FillCuUpValues(std::string plmId);
 
-    void AddCuUpUePmItem(std::string ueImsiComplete, long txPdcpPduBytesNrRlc, long txPdcpPduNrRlc);
+    void AddCuUpUePmItem(std::string ueImsiComplete, long txPdcpPduBytesNrRlc, long txPdcpPduNrRlc, double pdcpThroughput);
 
     void FillCuCpValues(uint16_t numActiveUes);
 
