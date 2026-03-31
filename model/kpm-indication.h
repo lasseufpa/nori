e513c4b9e5785f07d11b4584aed500bc57e040f4
Cleverson Nahum <cleversonahum@gmail.com>
2026-03-31 16:31:01 +0000

    update indication message and indication header

--- model/kpm-indication.h
+++ model/kpm-indication.h
@@ -24,12 +24,6 @@ extern "C"
 #include "E2SM-KPM-IndicationHeader.h"
 #include "E2SM-KPM-IndicationMessage.h"
 #include "E2SM-KPM-RANfunction-Description.h"
-#include "OCUCP-PF-Container.h"
-#include "OCUUP-PF-Container.h"
-#include "ODU-PF-Container.h"
-#include "PF-Container.h"
-#include "PF-ContainerListItem.h"
-#include "RAN-Container.h"
 #include "asn1c-types.h"
 }
 
@@ -39,14 +33,6 @@ namespace ns3
 class KpmIndicationHeader : public SimpleRefCount<KpmIndicationHeader>
 {
   public:
-    enum GlobalE2nodeType
-    {
-        gNB = 0,
-        eNB = 1,
-        ng_eNB = 2,
-        en_gNB = 3
-    };
-
     const int TIMESTAMP_LIMIT_SIZE = 8;
 
     /**
@@ -56,42 +42,19 @@ class KpmIndicationHeader : public SimpleRefCount<KpmIndicationHeader>
     {
         // E2SM-KPM Indication Header Format 1
         // KPM Node ID IE
-        std::string m_gnbId; //!< gNB ID bit string
-        // TODO not supported
-        // uint64_t m_cuUpId; //!< gNB-CU-UP ID, integer [0, 2^36-1], optional
+        std::string m_fileFormatVersion; //Optional, PrintableString
 
-        // Cell Global ID (NR CGI) IE
-        uint16_t m_nrCellId; //!< NR, bit string
+        std::string m_senderName; //Optional, PrintableString
 
-        // PLMN ID IE
-        std::string m_plmId; //!< PLMN identity, octet string, 3 bytes
+        std::string m_senderType; //Optional, PrintableString
 
-        // Slice ID (S-NSSAI) IE // TODO not supported
-        // std::string m_sst; //!< SNSSAI sST, 1 byte
-        // std::string m_sd; //!< SNSSAI sD, 3 bytes, optional
-
-        // FiveQI IE // TODO not supported
-        // uint8_t m_fiveqi; //!< fiveQI, integer [0, 255], optional
-
-        // QCI IE // TODO not supported
-        // long m_qci; //!< QCI, integer [0, 255], optional
-
-        // TODO this value is placed in a fiels which seems not to be defined
-        // in the specs. See line 301 in encode_kpm.cpp
-        // the field is called gNB_DU_ID
-        // it should be part of KPM Node ID IE
-        // m_duId
-
-        // TODO this value is placed in a fiels which seems not to be defined
-        // in the specs. See line 290 in encode_kpm.cpp, the field is called
-        // gNB_Name
-        // m_cuUpName
+        std::string m_vendorName; //Optional, PrintableString
 
         // CollectionTimeStamp
         uint64_t m_timestamp;
     };
 
-    KpmIndicationHeader(GlobalE2nodeType nodeType, KpmRicIndicationHeaderValues values);
+    KpmIndicationHeader(KpmRicIndicationHeaderValues values);
     ~KpmIndicationHeader();
     void* m_buffer;
     size_t m_size;
@@ -110,116 +73,36 @@ class KpmIndicationHeader : public SimpleRefCount<KpmIndicationHeader>
 
     void Encode(E2SM_KPM_IndicationHeader_t* descriptor);
 
-    GlobalE2nodeType m_nodeType;
 };
 
-class MeasurementItemList : public SimpleRefCount<MeasurementItemList>
+struct KpmMeasurementLabelValues
 {
-  private:
-    Ptr<OctetString>
-        m_id; // ID, contains the UE IMSI if used to carry UE-specific measurement items
-    std::vector<Ptr<MeasurementItem>> m_items; //!< list of Measurement Information Items
-  public:
-    MeasurementItemList();
-    MeasurementItemList(std::string ueId);
-    ~MeasurementItemList();
+    // Opcionals
+    std::string m_plmId;      //ServedPlmnPerCell on old version
+    uint64_t m_nrCellId;      //CellResourceReport on old version
+    uint8_t m_fiveQi;         //FiveGcDuPmContainer on old version
+    uint8_t m_qci;            // EpcDuPmContainer on old version
+    uint8_t m_qfi;            
+    uint32_t m_sNssai;       // Slice ID
+    uint32_t m_qciMax;
+    uint32_t m_qciMin;
+    uint32_t m_arpMax;
 
-    // NOTE defined here to avoid undefined references
-    template <class T>
-    void AddItem(std::string name, T value)
-    {
-        Ptr<MeasurementItem> item = Create<MeasurementItem>(name, value);
-        m_items.push_back(item);
-    }
-
-    std::vector<Ptr<MeasurementItem>> GetItems();
-    OCTET_STRING_t GetId();
 };
 
-/**
- * Base class to carry PM Container values
- */
-class PmContainerValues : public SimpleRefCount<PmContainerValues>
+struct KpmMeasurementInfoItemValues
 {
-  public:
-    virtual ~PmContainerValues() = default;
+    std::string m_measName;                // Ex: "RRU.PrbUsedDl"
+    uint16_t m_measID;
+    KpmMeasurementLabelValues m_labels;    //5QI, CellID, etc
 };
 
-/**
- * Contains the values to be inserted in the O-CU-CP Measurement Container
- */
-class OCuCpContainerValues : public PmContainerValues
+struct KpmMeasurementRecordValues
 {
-  public:
-    uint16_t m_numActiveUes; //!< mean number of RRC connections
+    //the order is the same of m_measInfoList
+    std::vector<double> m_values; 
 };
 
-/**
- * Contains the values to be inserted in the O-CU-UP Measurement Container
- */
-class OCuUpContainerValues : public PmContainerValues
-{
-  public:
-    std::string m_plmId; //!< PLMN identity, octet string, 3 bytes
-    long m_pDCPBytesUL;  //!< total PDCP bytes transmitted UL
-    long m_pDCPBytesDL;  //!< total PDCP bytes transmitted DL
-};
-
-/**
- * Contains the values to be inserted in the O-DU EPC Measurement Container
- */
-class EpcDuPmContainer : public SimpleRefCount<EpcDuPmContainer>
-{
-  public:
-    long m_qci;        //!< QCI value
-    long m_dlPrbUsage; //!< Used number of PRBs in an average of DL for the monitored slice during
-                       //!< E2 reporting period
-    long m_ulPrbUsage; //!< Used number of PRBs in an average of UL for the monitored slice during
-                       //!< E2 reporting period
-    virtual ~EpcDuPmContainer() = default;
-};
-
-/**
- * Contains the values to be inserted in the O-DU 5GC Measurement Container
- */
-class FiveGcDuPmContainer : public SimpleRefCount<FiveGcDuPmContainer>
-{
-  public:
-    // Snssai m_sliceId; //!< S-NSSAI
-    long m_fiveQi;     //!< 5QI value
-    long m_dlPrbUsage; //!< Used number of PRBs in an average of DL for the monitored slice during
-                       //!< E2 reporting period
-    long m_ulPrbUsage; //!< Used number of PRBs in an average of UL for the monitored slice during
-                       //!< E2 reporting period
-    virtual ~FiveGcDuPmContainer() = default;
-};
-
-class ServedPlmnPerCell : public SimpleRefCount<ServedPlmnPerCell>
-{
-  public:
-    std::string m_plmId; //!< PLMN identity, octet string, 3 bytes
-    uint16_t m_nrCellId;
-    std::set<Ptr<EpcDuPmContainer>> m_perQciReportItems;
-};
-
-class CellResourceReport : public SimpleRefCount<CellResourceReport>
-{
-  public:
-    std::string m_plmId; //!< PLMN identity, octet string, 3 bytes
-    uint16_t m_nrCellId;
-    long dlAvailablePrbs;
-    long ulAvailablePrbs;
-    std::set<Ptr<ServedPlmnPerCell>> m_servedPlmnPerCellItems;
-};
-
-/**
- * Contains the values to be inserted in the O-DU Measurement Container
- */
-class ODuContainerValues : public PmContainerValues
-{
-  public:
-    std::set<Ptr<CellResourceReport>> m_cellResourceReportItems;
-};
 
 class KpmIndicationMessage : public SimpleRefCount<KpmIndicationMessage>
 {
@@ -229,13 +112,13 @@ class KpmIndicationMessage : public SimpleRefCount<KpmIndicationMessage>
      */
     struct KpmIndicationMessageValues
     {
-        std::string m_cellObjectId; //!< Cell Object ID
-        Ptr<PmContainerValues>
-            m_pmContainerValues; //!< struct containing values to be inserted in the PM Container
-        Ptr<MeasurementItemList>
-            m_cellMeasurementItems; //!< list of cell-specific Measurement Information Items
-        std::set<Ptr<MeasurementItemList>>
-            m_ueIndications; //!< list of Measurement Information Items
+      //Structure: measData > measInfoList > GranulPeriod
+
+
+      std::vector<KpmMeasurementRecordValues> m_measData; //Measurements Record. Mandatory
+      std::vector<KpmMeasurementInfoItemValues> m_measInfoList; //Measurement Information List. Mandatory
+      uint32_t m_granularityPeriod; //Granularity Period. Optional 
+
     };
 
     KpmIndicationMessage(KpmIndicationMessageValues values);
@@ -246,10 +129,8 @@ class KpmIndicationMessage : public SimpleRefCount<KpmIndicationMessage>
 
   private:
     static void CheckConstraints(KpmIndicationMessageValues values);
-    void FillPmContainer(PF_Container_t* ranContainer, Ptr<PmContainerValues> values);
-    void FillOCuUpContainer(PF_Container_t* ranContainer, Ptr<OCuUpContainerValues> values);
-    void FillOCuCpContainer(PF_Container_t* ranContainer, Ptr<OCuCpContainerValues> values);
-    void FillODuContainer(PF_Container_t* ranContainer, Ptr<ODuContainerValues> values);
+    void FillAndEncodeIndicationMessageFormat1(E2SM_KPM_IndicationMessage_Format1_t* format1, 
+                     KpmIndicationMessageValues values);
     void FillAndEncodeKpmIndicationMessage(E2SM_KPM_IndicationMessage_t* descriptor,
                                            KpmIndicationMessageValues values);
     void Encode(E2SM_KPM_IndicationMessage_t* descriptor);
