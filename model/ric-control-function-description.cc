bf67ab0cdd4f5c06ed0aa30e79fb9509051bded6
Andrey Adailso <andreyadailsom@gmail.com>
2026-09-01 17:10:18 +0000

    feat: Added support for defining the RAN function in E2SM-RC v3.01

--- model/ric-control-function-description.cc
+++ model/ric-control-function-description.cc
@@ -3,14 +3,14 @@
  * Copyright (c) 2022 Northeastern University
  * Copyright (c) 2022 Sapienza, University of Rome
  * Copyright (c) 2022 University of Padova
+ * Copyright (c) 2026 LASSE/UFPA - Universidade Federal do Pará
  *
  * SPDX-License-Identifier: GPL-2.0-only
- *
- *
  *
  * Author: Andrea Lacava <thecave003@gmail.com>
  *         Tommaso Zugno <tommasozugno@gmail.com>
  *         Michele Polese <michele.polese@gmail.com>
+ *         Andrey Adailso <andreyadailsom@gmail.com>
  */
 
 #include "ric-control-function-description.h"
@@ -21,9 +21,10 @@
 
 extern "C"
 {
-#include "RAN-ControlParameter-Item.h"
-#include "RIC-ControlAction-Item.h"
-#include "RIC-ControlStyle-Item.h"
+#include "ControlAction-RANParameter-Item.h"
+#include "RANFunctionDefinition-Control-Action-Item.h"
+#include "RANFunctionDefinition-Control-Item.h"
+#include "RANFunctionDefinition-Control.h"
 }
 
 namespace ns3
@@ -34,6 +35,7 @@ NS_LOG_COMPONENT_DEFINE("RicControlFunctionDescription");
 RicControlFunctionDescription::RicControlFunctionDescription()
 {
     E2SM_RC_RANFunctionDefinition_t* descriptor = new E2SM_RC_RANFunctionDefinition_t();
+    memset(descriptor, 0, sizeof(E2SM_RC_RANFunctionDefinition_t));
     FillAndEncodeRCFunctionDescription(descriptor);
     ASN_STRUCT_FREE_CONTENTS_ONLY(asn_DEF_E2SM_RC_RANFunctionDefinition, descriptor);
     delete descriptor;
@@ -41,13 +43,14 @@ RicControlFunctionDescription::RicControlFunctionDescription()
 
 RicControlFunctionDescription::~RicControlFunctionDescription()
 {
+    free(m_buffer);
+    m_size = 0;
 }
 
 void
 RicControlFunctionDescription::Encode(E2SM_RC_RANFunctionDefinition_t* descriptor)
 {
     asn_codec_ctx_t* opt_cod = 0; // disable stack bounds checking
-    // encode the structure into the e2smbuffer
     asn_encode_to_new_buffer_result_s encodedMsg =
         asn_encode_to_new_buffer(opt_cod,
                                  ATS_ALIGNED_BASIC_PER,
@@ -56,7 +59,7 @@ RicControlFunctionDescription::Encode(E2SM_RC_RANFunctionDefinition_t* descripto
 
     if (encodedMsg.result.encoded < 0)
     {
-        NS_FATAL_ERROR("Error during the encoding of the RIC Indication Header, errno: "
+        NS_FATAL_ERROR("Error during the encoding of the E2SM-RC RAN Function Definition, errno: "
                        << strerror(errno) << ", failed_type " << encodedMsg.result.failed_type->name
                        << ", structure_ptr " << encodedMsg.result.structure_ptr);
     }
@@ -65,172 +68,82 @@ RicControlFunctionDescription::Encode(E2SM_RC_RANFunctionDefinition_t* descripto
     m_size = encodedMsg.result.encoded;
 }
 
+static void
+AddControlActionRanParameter(
+    RANFunctionDefinition_Control_Action_Item::
+        RANFunctionDefinition_Control_Action_Item__ran_ControlActionParameters_List* list,
+    long paramId,
+    const std::string& paramName)
+{
+    auto* paramItem =
+        (ControlAction_RANParameter_Item_t*)calloc(1, sizeof(ControlAction_RANParameter_Item_t));
+    paramItem->ranParameter_ID = paramId;
+    OCTET_STRING_fromString(&paramItem->ranParameter_name, paramName.c_str());
+    ASN_SEQUENCE_ADD(&list->list, paramItem);
+}
+
 void
 RicControlFunctionDescription::FillAndEncodeRCFunctionDescription(
     E2SM_RC_RANFunctionDefinition_t* ranfunc_desc)
 {
-    std::string shortNameBuffer = "ORAN-WG3-RC";
+    const std::string shortName = "ORAN-E2SM-RC";
+    const std::string description = "RAN Control Service Model v3.01";
+    const std::string oid = "1.3.6.1.4.1.53148.1.3.2.3";
 
-    Ptr<OctetString> shortName = Create<OctetString>(shortNameBuffer, shortNameBuffer.size());
-
-    ranfunc_desc->ranFunction_Name.ranFunction_ShortName = shortName->GetValue();
-
-    // This part is not in the specs, maybe it can be removed?
-
-    uint8_t* descriptionBuffer = (uint8_t*)"RIC Control Definitions";
-    uint8_t* oidBuffer = (uint8_t*)"OID123"; // this is optional, dummy value
+    OCTET_STRING_fromString(&ranfunc_desc->ranFunction_Name.ranFunction_ShortName, shortName.c_str());
+    OCTET_STRING_fromString(&ranfunc_desc->ranFunction_Name.ranFunction_Description, description.c_str());
+    OCTET_STRING_fromString(&ranfunc_desc->ranFunction_Name.ranFunction_E2SM_OID, oid.c_str());
 
     long* inst = (long*)calloc(1, sizeof(long));
-
-    ranfunc_desc->ranFunction_Name.ranFunction_Description.buf =
-        (uint8_t*)calloc(1, strlen((char*)descriptionBuffer));
-    memcpy(ranfunc_desc->ranFunction_Name.ranFunction_Description.buf,
-           descriptionBuffer,
-           strlen((char*)descriptionBuffer));
-    ranfunc_desc->ranFunction_Name.ranFunction_Description.size = strlen((char*)descriptionBuffer);
+    *inst = 0;
     ranfunc_desc->ranFunction_Name.ranFunction_Instance = inst;
-    ranfunc_desc->ranFunction_Name.ranFunction_E2SM_OID.buf =
-        (uint8_t*)calloc(1, strlen((char*)oidBuffer));
-    memcpy(ranfunc_desc->ranFunction_Name.ranFunction_E2SM_OID.buf,
-           oidBuffer,
-           strlen((char*)oidBuffer));
-    ranfunc_desc->ranFunction_Name.ranFunction_E2SM_OID.size = strlen((char*)oidBuffer);
 
-    // End part of of specs
+    // RANFunctionDefinition-Control
+    ranfunc_desc->ranFunctionDefinition_Control =
+        (struct RANFunctionDefinition_Control*)calloc(1, sizeof(struct RANFunctionDefinition_Control));
 
-    RIC_ControlStyle_Item_t* control_style0 =
-        (RIC_ControlStyle_Item_t*)calloc(1, sizeof(RIC_ControlStyle_Item_t));
-    control_style0->ric_ControlStyle_Type = 1;
+    // Control Style 1: "Radio Bearer Control"
+    auto* controlStyle1 =
+        (RANFunctionDefinition_Control_Item_t*)calloc(1, sizeof(RANFunctionDefinition_Control_Item_t));
+    controlStyle1->ric_ControlStyle_Type = 1;
+    OCTET_STRING_fromString(&controlStyle1->ric_ControlStyle_Name, "Radio Bearer Control");
+    controlStyle1->ric_ControlHeaderFormat_Type = 1;
+    controlStyle1->ric_ControlMessageFormat_Type = 1;
+    controlStyle1->ric_ControlOutcomeFormat_Type = 1;
 
-    uint8_t* controlStyleName0Buffer = (uint8_t*)"Radio Bearer Control";
-    size_t controlStyleName0BufferSize = strlen((char*)controlStyleName0Buffer);
-    control_style0->ric_ControlStyle_Name.buf = (uint8_t*)calloc(1, controlStyleName0BufferSize);
-    memcpy(control_style0->ric_ControlStyle_Name.buf,
-           controlStyleName0Buffer,
-           controlStyleName0BufferSize);
-    control_style0->ric_ControlStyle_Name.size = controlStyleName0BufferSize;
-
-    // these two were not present in the spec in this control style but just in the second, remove
-    // if they lead to crash
-    control_style0->ric_ControlMessageFormat_Type = 1;
-    control_style0->ric_ControlHeaderFormat_Type = 1;
-
-    control_style0->ric_ControlAction_List =
-        (RIC_ControlStyle_Item::RIC_ControlStyle_Item__ric_ControlAction_List*)
-            calloc(1, sizeof(RIC_ControlStyle_Item::RIC_ControlStyle_Item__ric_ControlAction_List));
-
-    RIC_ControlAction_Item_t* control_action0 =
-        (RIC_ControlAction_Item_t*)calloc(1, sizeof(RIC_ControlAction_Item_t));
-
-    control_action0->ric_ControlAction_ID = 6;
-    uint8_t* controlActionName0Buffer = (uint8_t*)"DRB split ratio control";
-    size_t controlActionName0BufferSize = strlen((char*)controlActionName0Buffer);
-    control_action0->ric_ControlAction_Name.buf = (uint8_t*)calloc(1, controlActionName0BufferSize);
-    memcpy(control_action0->ric_ControlAction_Name.buf,
-           controlActionName0Buffer,
-           controlActionName0BufferSize);
-    control_action0->ric_ControlAction_Name.size = controlActionName0BufferSize;
-    control_action0->ran_ControlParameters_List =
-        (RIC_ControlAction_Item::RIC_ControlAction_Item__ran_ControlParameters_List*)calloc(
-            1,
-            sizeof(RIC_ControlAction_Item::RIC_ControlAction_Item__ran_ControlParameters_List));
-
-    RAN_ControlParameter_Item_t* ranParameter0 =
-        (RAN_ControlParameter_Item_t*)calloc(1, sizeof(RAN_ControlParameter_Item_t));
-    uint8_t* ranParameter0NameBuffer = (uint8_t*)"Downlink PDCP Data Split";
-    size_t ranParameter0NameBufferSize = strlen((char*)ranParameter0NameBuffer);
-    ranParameter0->ranParameter_Name.buf = (uint8_t*)calloc(1, ranParameter0NameBufferSize);
-    memcpy(ranParameter0->ranParameter_Name.buf,
-           ranParameter0NameBuffer,
-           ranParameter0NameBufferSize);
-    ranParameter0->ranParameter_Name.size = ranParameter0NameBufferSize;
-    ranParameter0->ranParameter_ID = 3;
-
-    RAN_ControlParameter_Item_t* ranParameter1 =
-        (RAN_ControlParameter_Item_t*)calloc(1, sizeof(RAN_ControlParameter_Item_t));
-    uint8_t* ranParameter1NameBuffer = (uint8_t*)"Uplink PDCP Data Split Threshold";
-    size_t ranParameter1NameBufferSize = strlen((char*)ranParameter1NameBuffer);
-    ranParameter1->ranParameter_Name.buf = (uint8_t*)calloc(1, ranParameter1NameBufferSize);
-    memcpy(ranParameter1->ranParameter_Name.buf,
-           ranParameter1NameBuffer,
-           ranParameter1NameBufferSize);
-    ranParameter1->ranParameter_ID = 2;
-
-    ASN_SEQUENCE_ADD(&control_action0->ran_ControlParameters_List->list, ranParameter0);
-    ASN_SEQUENCE_ADD(&control_action0->ran_ControlParameters_List->list, ranParameter1);
-
-    ASN_SEQUENCE_ADD(&control_style0->ric_ControlAction_List->list, control_action0);
-
-    RIC_ControlStyle_Item_t* control_style1 =
-        (RIC_ControlStyle_Item_t*)calloc(1, sizeof(RIC_ControlStyle_Item_t));
-    control_style1->ric_ControlStyle_Type = 3;
-
-    uint8_t* controlStyleName1Buffer = (uint8_t*)"Radio Bearer Control";
-    size_t controlStyleName1BufferSize = strlen((char*)controlStyleName1Buffer);
-    control_style1->ric_ControlStyle_Name.buf = (uint8_t*)calloc(1, controlStyleName1BufferSize);
-    memcpy(control_style1->ric_ControlStyle_Name.buf,
-           controlStyleName1Buffer,
-           controlStyleName1BufferSize);
-    control_style1->ric_ControlStyle_Name.size = controlStyleName1BufferSize;
-
-    control_style1->ric_ControlMessageFormat_Type = 1;
-    control_style1->ric_ControlHeaderFormat_Type = 1;
-
-    control_style1->ric_ControlAction_List =
-        (RIC_ControlStyle_Item::RIC_ControlStyle_Item__ric_ControlAction_List*)
-            calloc(1, sizeof(RIC_ControlStyle_Item::RIC_ControlStyle_Item__ric_ControlAction_List));
-
-    RIC_ControlAction_Item_t* control_action1 =
-        (RIC_ControlAction_Item_t*)calloc(1, sizeof(RIC_ControlAction_Item_t));
-
-    control_action1->ric_ControlAction_ID = 1;
-    uint8_t* controlActionName1Buffer = (uint8_t*)"Handover control";
-    size_t controlActionName1BufferSize = strlen((char*)controlActionName1Buffer);
-    control_action1->ric_ControlAction_Name.buf = (uint8_t*)calloc(1, controlActionName1BufferSize);
-    memcpy(control_action1->ric_ControlAction_Name.buf,
-           controlActionName1Buffer,
-           controlActionName1BufferSize);
-    control_action1->ric_ControlAction_Name.size = controlActionName1BufferSize;
-    control_action1->ran_ControlParameters_List =
-        (RIC_ControlAction_Item::RIC_ControlAction_Item__ran_ControlParameters_List*)calloc(
-            1,
-            sizeof(RIC_ControlAction_Item::RIC_ControlAction_Item__ran_ControlParameters_List));
-
-    RAN_ControlParameter_Item_t* ranParameter2 =
-        (RAN_ControlParameter_Item_t*)calloc(1, sizeof(RAN_ControlParameter_Item_t));
-    uint8_t* ranParameter2NameBuffer = (uint8_t*)"NR CGI";
-    size_t ranParameter2NameBufferSize = strlen((char*)ranParameter2NameBuffer);
-    ranParameter2->ranParameter_Name.buf = (uint8_t*)calloc(1, ranParameter2NameBufferSize);
-    memcpy(ranParameter2->ranParameter_Name.buf,
-           ranParameter2NameBuffer,
-           ranParameter2NameBufferSize);
-    ranParameter2->ranParameter_Name.size = ranParameter2NameBufferSize;
-    ranParameter2->ranParameter_ID = 4;
-
-    RAN_ControlParameter_Item_t* ranParameter3 =
-        (RAN_ControlParameter_Item_t*)calloc(1, sizeof(RAN_ControlParameter_Item_t));
-    uint8_t* ranParameter3NameBuffer = (uint8_t*)"E-UTRA CGI";
-    size_t ranParameter3NameBufferSize = strlen((char*)ranParameter3NameBuffer);
-    ranParameter3->ranParameter_Name.buf = (uint8_t*)calloc(1, ranParameter3NameBufferSize);
-    memcpy(ranParameter3->ranParameter_Name.buf,
-           ranParameter3NameBuffer,
-           ranParameter3NameBufferSize);
-    ranParameter3->ranParameter_Name.size = ranParameter3NameBufferSize;
-    ranParameter3->ranParameter_ID = 6;
-
-    ASN_SEQUENCE_ADD(&control_action1->ran_ControlParameters_List->list, ranParameter2);
-    ASN_SEQUENCE_ADD(&control_action1->ran_ControlParameters_List->list, ranParameter3);
-
-    ASN_SEQUENCE_ADD(&control_style1->ric_ControlAction_List->list, control_action1);
-
-    ranfunc_desc->ric_ControlStyle_List =
-        (E2SM_RC_RANFunctionDefinition::E2SM_RC_RANFunctionDefinition__ric_ControlStyle_List*)
+    controlStyle1->ric_ControlAction_List =
+        (RANFunctionDefinition_Control_Item::
+             RANFunctionDefinition_Control_Item__ric_ControlAction_List*)
             calloc(1,
-                   sizeof(E2SM_RC_RANFunctionDefinition::
-                              E2SM_RC_RANFunctionDefinition__ric_ControlStyle_List));
+                   sizeof(RANFunctionDefinition_Control_Item::
+                              RANFunctionDefinition_Control_Item__ric_ControlAction_List));
 
-    ASN_SEQUENCE_ADD(&ranfunc_desc->ric_ControlStyle_List->list, control_style0);
-    ASN_SEQUENCE_ADD(&ranfunc_desc->ric_ControlStyle_List->list, control_style1);
+    // Control Action ID 6: "Slice-level PRB quota"
+    auto* controlAction6 = (RANFunctionDefinition_Control_Action_Item_t*)calloc(
+        1,
+        sizeof(RANFunctionDefinition_Control_Action_Item_t));
+    controlAction6->ric_ControlAction_ID = 6;
+    OCTET_STRING_fromString(&controlAction6->ric_ControlAction_Name, "Slice-level PRB quota");
+
+    controlAction6->ran_ControlActionParameters_List =
+        (RANFunctionDefinition_Control_Action_Item::
+             RANFunctionDefinition_Control_Action_Item__ran_ControlActionParameters_List*)
+            calloc(1,
+                   sizeof(RANFunctionDefinition_Control_Action_Item::
+                              RANFunctionDefinition_Control_Action_Item__ran_ControlActionParameters_List));
+
+    // RAN Parameters for Action 6 (Slice-level PRB quota)
+    AddControlActionRanParameter(controlAction6->ran_ControlActionParameters_List, 1, "RRM Policy Ratio List");
+    AddControlActionRanParameter(controlAction6->ran_ControlActionParameters_List, 2, "RRM Policy Ratio Group");
+    AddControlActionRanParameter(controlAction6->ran_ControlActionParameters_List, 3, "RRM Policy SST");
+    AddControlActionRanParameter(controlAction6->ran_ControlActionParameters_List, 4, "Max PRB Policy Ratio");
+    AddControlActionRanParameter(controlAction6->ran_ControlActionParameters_List, 5, "Min PRB Policy Ratio");
+    AddControlActionRanParameter(controlAction6->ran_ControlActionParameters_List, 6, "Dedicated PRB Policy Ratio");
+
+    ASN_SEQUENCE_ADD(&controlStyle1->ric_ControlAction_List->list, controlAction6);
+
+    ASN_SEQUENCE_ADD(&ranfunc_desc->ranFunctionDefinition_Control->ric_ControlStyle_List.list,
+                     controlStyle1);
 
     Encode(ranfunc_desc);
 
--- model/ric-control-function-description.h
+++ model/ric-control-function-description.h
@@ -3,14 +3,14 @@
  * Copyright (c) 2022 Northeastern University
  * Copyright (c) 2022 Sapienza, University of Rome
  * Copyright (c) 2022 University of Padova
+ * Copyright (c) 2026 LASSE/UFPA - Universidade Federal do Pará
  *
  * SPDX-License-Identifier: GPL-2.0-only
- *
- *
  *
  * Author: Andrea Lacava <thecave003@gmail.com>
  *         Tommaso Zugno <tommasozugno@gmail.com>
  *         Michele Polese <michele.polese@gmail.com>
+ *         Andrey Adailso <andreyadailsom@gmail.com>
  */
 
 #pragma once
@@ -22,6 +22,10 @@
 extern "C"
 {
 #include "E2SM-RC-RANFunctionDefinition.h"
+#include "RANFunctionDefinition-Control.h"
+#include "RANFunctionDefinition-Control-Item.h"
+#include "RANFunctionDefinition-Control-Action-Item.h"
+#include "ControlAction-RANParameter-Item.h"
 }
 
 namespace ns3
@@ -34,9 +38,13 @@ class RicControlFunctionDescription : public FunctionDescription
     ~RicControlFunctionDescription();
 
   private:
+    /**
+     * Encodes the RAN Function Definition item for the E2SM-RC Service Model v3.01.
+     *
+     * @param descriptor the RAN Function Definition item
+     */
     void FillAndEncodeRCFunctionDescription(E2SM_RC_RANFunctionDefinition_t* descriptor);
     void Encode(E2SM_RC_RANFunctionDefinition_t* descriptor);
 };
-} // namespace ns3
 
-#pragma once
+} // namespace ns3
